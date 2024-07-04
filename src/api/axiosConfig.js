bc1f8f95267421f0566333ccbb320207b433366b
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2024-07-03 23:43:25 -0500

    set url to production

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL:
+    "http://note-taking-app-env.eba-iutjphmt.us-east-1.elasticbeanstalk.com/",
 });
 
 export default api;
